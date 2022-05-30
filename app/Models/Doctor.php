55cba3a35a9476de852c4cdfab311f33bff2934b
mohosin16103308 <hamza1610330816@gmail.com>
2022-05-31 01:59:19 +0600

    Doctor Model functionality added

--- app/Models/Doctor.php
+++ app/Models/Doctor.php
@@ -7,8 +7,32 @@ use Illuminate\Database\Eloquent\Model;
 
 class Doctor extends Model
 {
-    use HasFactory, SoftDeletes;
+  use HasFactory, SoftDeletes;
 
     protected $table = 'doctors';
 
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+
+    protected $fillable = [
+        'name',
+        'phone',
+        'email',
+        'designation',
+        'personal_home_page',
+        'degress',
+        'department',
+        'specialist',
+        'experience',
+        'date_of_birth',
+        'gender',
+        'blood_group',
+        'about_me',
+        'profile_pic',
+        'password',
+        'address',
+    ];
 }
