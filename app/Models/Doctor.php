ec96692fe2ee0177c04f4694c09455c380eb136b
mohosin16103308 <hamza1610330816@gmail.com>
2022-05-31 01:42:59 +0600

    Migration Added For Doctor

--- app/Models/Doctor.php
+++ app/Models/Doctor.php
@@ -7,5 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Doctor extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+
 }
--- database/migrations/2022_05_30_192752_create_doctors_table.php
+++ database/migrations/2022_05_30_192752_create_doctors_table.php
@@ -15,6 +15,25 @@ return new class extends Migration
     {
         Schema::create('doctors', function (Blueprint $table) {
             $table->id();
+
+            $table->string('name');
+            $table->string('phone');
+            $table->string('email')->nullable();;
+            $table->string('designation')->nullable();;
+            $table->string('personal_home_page')->nullable();;
+            $table->string('degress')->nullable();;
+            $table->string('department')->nullable();;
+            $table->string('specialist')->nullable();;
+            $table->string('experience')->nullable();;
+            $table->string('date_of_birth')->nullable();;
+            $table->string('gender')->nullable();;
+            $table->string('blood_group')->nullable();;
+            $table->string('address')->nullable();;
+            $table->string('about_me')->nullable();;
+            $table->string('profile_pic')->nullable();;
+            $table->string('password');
+            
+            $table->softDeletes();
             $table->timestamps();
         });
     }
