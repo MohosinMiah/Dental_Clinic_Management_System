bd9c8ed921a1cefd82dce16da0d49e65520a8eea
mohosin16103308 <hamza1610330816@gmail.com>
2022-06-07 00:49:38 +0600

    Invoice Schema Added Verson 2

--- database/migrations/2022_06_01_195252_create_notices_table.php
+++ database/migrations/2022_06_01_195252_create_notices_table.php
@@ -14,36 +14,14 @@ return new class extends Migration
     public function up()
     {
         Schema::create('notices', function (Blueprint $table) {
-
             $table->id();
 
-            $table->string('patient_id');
-            $table->text('doctor_id')->nullable();
-            $table->text('service_id')->nullable();
-            $table->text('added_by_id')->nullable();
-
-            $table->string('patient_phone');
-            $table->text('patient_address')->nullable();
-
-            $table->text('payment_date');
-
-			$table->float('total')->default(0);
-			$table->float('tax_total')->default(0);
-			$table->float('grand_total')->default(0);
-			$table->float('paid_amount')->default(0);
-			$table->float('due_total')->default(0);
-
-
-            $table->string('payment_note')->nullable();
-            $table->string('payment_method')->default('Cash');
-            $table->string('payment_method_note')->nullable();
-
-			// If User Personal Mobile for Bkash, Nagot or Other Mobile Banking Services
-
+            $table->string('title');
+            $table->text('description');
+            $table->string('image')->nullable();;
+            $table->integer()->default(0);
             $table->softDeletes();
             $table->timestamps();
-
-
         });
     }
 
--- database/migrations/2022_06_06_151905_create_invoices_table.php
+++ database/migrations/2022_06_06_151905_create_invoices_table.php
@@ -15,6 +15,31 @@ return new class extends Migration
     {
         Schema::create('invoices', function (Blueprint $table) {
             $table->id();
+
+            $table->string('patient_id');
+            $table->text('doctor_id')->nullable();
+            $table->text('service_id')->nullable();
+            $table->text('added_by_id')->nullable();
+
+            $table->string('patient_phone');
+            $table->text('patient_address')->nullable();
+
+            $table->text('payment_date');
+
+			$table->float('total')->default(0);
+			$table->float('tax_total')->default(0);
+			$table->float('grand_total')->default(0);
+			$table->float('paid_amount')->default(0);
+			$table->float('due_total')->default(0);
+
+
+            $table->string('payment_note')->nullable();
+            $table->string('payment_method')->default('Cash');
+            $table->string('payment_method_note')->nullable();
+
+			// If User Personal Mobile for Bkash, Nagot or Other Mobile Banking Services
+
+            $table->softDeletes();
             $table->timestamps();
         });
     }
