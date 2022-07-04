2553a01a3ad0a03b396979bb4717fd3c3a69c9df
mohosin16103308 <hamza1610330816@gmail.com>
2022-07-04 22:23:22 +0600

    Appointment Schema Created

--- database/migrations/2022_07_04_161709_create_appointments_table.php
+++ database/migrations/2022_07_04_161709_create_appointments_table.php
@@ -15,6 +15,14 @@ return new class extends Migration
     {
         Schema::create('appointments', function (Blueprint $table) {
             $table->id();
+            $table->string('patient_phone');
+			$table->string('date');
+			$table->string('gender')->nullable();
+            $table->string('email')->nullable();
+			$table->integer('doctor_id');
+            $table->string('note')->nullable();
+            $table->string('isRegistered')->nullable();
+            $table->softDeletes();
             $table->timestamps();
         });
     }
