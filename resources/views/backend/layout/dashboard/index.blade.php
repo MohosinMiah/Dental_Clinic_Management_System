651bebfedfa88da58a03fbc8d9de065c4986fe58
mohosin16103308 <hamza1610330816@gmail.com>
2022-07-27 01:43:30 +0600

    Updated Index

--- resources/views/backend/layout/dashboard/index.blade.php
+++ resources/views/backend/layout/dashboard/index.blade.php
@@ -7,43 +7,59 @@
 	<div class="row">
 		<div class="col-md-3">
 			<div class="card" style="width: 18rem;">
-				<img class="card-img-top" src="https://jupiterplatform.com/images/icons/lead-central.png" alt="Card image cap">
+				<a href="{{ route( 'patient_list') }}">
+				<img class="card-img-top" src="/images/settings.png" alt="Card image cap" />
+				</a>
 				<div class="card-body">
-				  <a href="#" class="btn btn-primary">Patients</a>
+				  <a href="{{ route( 'patient_list') }}" class="btn btn-primary">Patients</a>
 				</div>
 			  </div>
 		</div>
 		
 		<div class="col-md-3">
 			<div class="card" style="width: 18rem;">
-				<img class="card-img-top" src="https://jupiterplatform.com/images/icons/lead-central.png" alt="Card image cap">
+				<a href="{{ route( 'appointment_list') }}">
+				<img class="card-img-top" src="/images/settings.png" alt="Card image cap" />
+				</a>
 				<div class="card-body">
-				  <a href="#" class="btn btn-primary">Doctors</a>
+				  <a href="{{ route( 'appointment_list') }}" class="btn btn-primary">Appointments</a>
+				</div>
+			  </div>
+		</div>
+
+	
+
+		<div class="col-md-3">
+			
+			<div class="card" style="width: 18rem;">
+				<a href="{{ route( 'notice_list') }}">
+				<img class="card-img-top" src="/images/settings.png" alt="Card image cap" />
+				</a>
+				<div class="card-body">
+				  <a href="{{ route( 'notice_list') }}" class="btn btn-primary">Notices</a>
 				</div>
 			  </div>
 		</div>
 
 		<div class="col-md-3">
 			<div class="card" style="width: 18rem;">
-				<img class="card-img-top" src="https://jupiterplatform.com/images/icons/lead-central.png" alt="Card image cap">
+				<a href="{{ route( 'invoice_list') }}">
+				<img class="card-img-top" src="/images/settings.png" alt="Card image cap" />
+				</a>
 				<div class="card-body">
-				  <a href="#" class="btn btn-primary">Invoices</a>
+				  <a href="{{ route( 'invoice_list' ) }}" class="btn btn-primary">Invoices</a>
 				</div>
 			  </div>
 		</div>
+
+	
 		<div class="col-md-3">
 			<div class="card" style="width: 18rem;">
-				<img class="card-img-top" src="https://jupiterplatform.com/images/icons/lead-central.png" alt="Card image cap">
+				<a href="{{ route( 'doctor_list') }}">
+				<img class="card-img-top" src="/images/settings.png" alt="Card image cap" />
+				</a>
 				<div class="card-body">
-				  <a href="#" class="btn btn-primary">Go somewhere</a>
-				</div>
-			  </div>
-		</div>
-		<div class="col-md-3">
-			<div class="card" style="width: 18rem;">
-				<img class="card-img-top" src="https://jupiterplatform.com/images/icons/lead-central.png" alt="Card image cap">
-				<div class="card-body">
-				  <a href="#" class="btn btn-primary">Go somewhere</a>
+				  <a href="{{ route( 'doctor_list' ) }}" class="btn btn-primary">Doctors</a>
 				</div>
 			  </div>
 		</div>
