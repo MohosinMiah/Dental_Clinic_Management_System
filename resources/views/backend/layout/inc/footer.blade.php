c6c3a374400e731c4cb75d8beb3aea71f7da7b8c
mohosin16103308 <hamza1610330816@gmail.com>
2022-08-04 22:13:09 +0600

    Updated Database

--- resources/views/backend/layout/inc/footer.blade.php
+++ resources/views/backend/layout/inc/footer.blade.php
@@ -7,7 +7,7 @@
 <footer class="sticky-footer bg-white">
     <div class="container my-auto">
         <div class="copyright text-center my-auto">
-            <span>Copyright &copy; Your Website 2020</span>
+            <span>Copyright &copy; MonkeykingBD. <span style="color: green">Design and Developed By</span> <a href="http://monkeykingbd.com" target="_blank" style="color:red">Monkey Kind BD</a></span>
         </div>
     </div>
 </footer>
